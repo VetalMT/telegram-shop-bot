78966e170b4c22ca594905daa82bbbbd9d7bb255
VetalMT <pelene2017@gmail.com>
2025-08-22 03:52:49 +0300

    keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,19 +2,19 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # Клавіатура для користувача
 shop_kb = ReplyKeyboardMarkup(
+    resize_keyboard=True,
     keyboard=[
-        [KeyboardButton("🛍 Переглянути товари")],
-        [KeyboardButton("🛒 Корзина")]
-    ],
-    resize_keyboard=True
+        [KeyboardButton(text="🛍 Переглянути товари")],
+        [KeyboardButton(text="🛒 Корзина")]
+    ]
 )
 
 # Клавіатура для адміна
 admin_kb = ReplyKeyboardMarkup(
+    resize_keyboard=True,
     keyboard=[
-        [KeyboardButton("➕ Додати товар")],
-        [KeyboardButton("❌ Видалити товар")],
-        [KeyboardButton("📦 Переглянути товари")]
-    ],
-    resize_keyboard=True
+        [KeyboardButton(text="➕ Додати товар")],
+        [KeyboardButton(text="❌ Видалити товар")],
+        [KeyboardButton(text="📦 Переглянути товари")]
+    ]
 )
