a3b69f6d1deed708d702a09e039200ee3715ba91
VetalMT <pelene2017@gmail.com>
2025-08-23 15:40:24 +0300

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "123456789"))  # додай свій у Render env
+ADMIN_ID = int(os.getenv("ADMIN_ID", "469040941"))  # додай свій у Render env
 DB_PATH = "shop.db"
