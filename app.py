97765663a8cdfc8d25bcd1628db1d8c49e4a5366
VetalMT <pelene2017@gmail.com>
2025-08-27 20:53:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import logging
 from aiohttp import web
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-
 from config import BOT_TOKEN, RENDER_EXTERNAL_URL, PORT
 from handlers_admin import admin_router
 from handlers_user import user_router
@@ -13,12 +11,14 @@ WEBHOOK_URL = f"{RENDER_EXTERNAL_URL}/webhook"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
 dp.include_router(admin_router)
 dp.include_router(user_router)
 
-@dp.message(Command("ping"))
+@dp.message()
 async def ping(message: types.Message):
-    await message.answer("pong 🟢")
+    if message.text == "/ping":
+        await message.answer("pong 🟢")
 
 async def handle_webhook(request: web.Request):
     update = await request.json()
