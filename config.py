eb15b670f00fe99ce3fac2df99a84daec6306e52
VetalMT <pelene2017@gmail.com>
2025-08-27 23:26:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Токен твого бота
-DATABASE_URL = os.getenv("DATABASE_URL")  # URL до PostgreSQL Render
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL")  # https://your-app.onrender.com
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+DB_HOST = os.getenv("DB_HOST", "")
+DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_PORT = int(os.getenv("DB_PORT", 5432))
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "")
 PORT = int(os.getenv("PORT", 10000))
-
-ADMIN_ID = int(os.getenv("ADMIN_ID", 123456789))  # твій Telegram ID
