bf85011cf195ebe2153da3e7faba8b137a3edb6e
VetalMT <pelene2017@gmail.com>
2025-08-27 21:14:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://shop_db_j2lo_user:password@dpg-d2n2u92li9vc73chmkqg-a:5432/shop_db_j2lo")
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://telegram-shop-bot-z03b.onrender.com")
-PORT = int(os.getenv("PORT", 8000))
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Токен твого бота
+DATABASE_URL = os.getenv("DATABASE_URL")  # URL до PostgreSQL Render
+RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL")  # https://your-app.onrender.com
+PORT = int(os.getenv("PORT", 10000))
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", 123456789))  # твій Telegram ID
