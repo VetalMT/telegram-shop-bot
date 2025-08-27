3c513d54ec6221fe0c8b7f93da73d1eb89602b71
VetalMT <pelene2017@gmail.com>
2025-08-27 20:51:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "ВАШ_ТОКЕН_БОТА")
 PORT = int(os.getenv("PORT", 8000))
+RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://your-app.onrender.com")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://shop_db_j2lo_user:password@dpg-d2n2u92li9vc73chmkqg-a:5432/shop_db_j2lo")
