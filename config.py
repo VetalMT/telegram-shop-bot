3b90bbc159b28f97fdc8bd89904ca156591e05d9
VetalMT <pelene2017@gmail.com>
2025-08-27 20:57:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "ВАШ_ТОКЕН_БОТА")
-PORT = int(os.getenv("PORT", 8000))
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://your-app.onrender.com")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://shop_db_j2lo_user:password@dpg-d2n2u92li9vc73chmkqg-a:5432/shop_db_j2lo")
+RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://telegram-shop-bot-z03b.onrender.com")
+PORT = int(os.getenv("PORT", 8000))
