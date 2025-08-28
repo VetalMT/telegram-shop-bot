1c1c99f6e262a84a850490cf41d740d8ca713975
VetalMT <pelene2017@gmail.com>
2025-08-29 00:49:55 +0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,29 @@
+import psycopg2
+import os
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            user_id BIGINT UNIQUE,
+            username TEXT
+        );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
