811bd94a5b45d12b510399d63fb0f59f1eb30f99
VetalMT <pelene2017@gmail.com>
2025-08-30 01:44:19 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float, LargeBinary, ForeignKey
-from sqlalchemy.orm import relationship
-from db import Base
-
-class Product(Base):
-    __tablename__ = "products"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String)
-    price = Column(Float, nullable=False)
-    photo = Column(LargeBinary)
-
-class CartItem(Base):
-    __tablename__ = "cart_items"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
-    product_id = Column(Integer, ForeignKey("products.id"))
-    quantity = Column(Integer, default=1)
-
-    product = relationship("Product")
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    price TEXT,
+    photo_id TEXT
+);
