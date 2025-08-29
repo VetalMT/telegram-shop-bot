b62800983ee87f2bbe12244a696db0468cabf1ff
VetalMT <pelene2017@gmail.com>
2025-08-29 22:44:07 +0300

    Update handlers_shop.py

--- handlers_shop.py
+++ handlers_shop.py
@@ -1,8 +1,8 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
-shop_router = Router()
+shop_router = Router(name="shop")
 
 @shop_router.message(Command("shop"))
 async def shop_start(message: types.Message):
-    await message.answer("🛍️ Ласкаво просимо в магазин!")
+    await message.answer("🛍️ Ласкаво просимо в магазин!\nВикористовуйте кнопки нижче ⬇️")
