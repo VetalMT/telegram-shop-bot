69f14a7127ca1416709760ebb0348cdb31c59c37
VetalMT <pelene2017@gmail.com>
2025-08-30 01:42:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-from config import DATABASE_URL
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
-
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://USER:PASSWORD@HOST:PORT/DBNAME"
+)
