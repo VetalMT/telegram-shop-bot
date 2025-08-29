8a7ba1744959056a26d4bbb4b47f47cdc22de264
VetalMT <pelene2017@gmail.com>
2025-08-30 01:37:09 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,20 @@
-вфівфі
+from sqlalchemy import Column, Integer, String, Float, LargeBinary, ForeignKey
+from sqlalchemy.orm import relationship
+from db import Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float, nullable=False)
+    photo = Column(LargeBinary)
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer, default=1)
+
+    product = relationship("Product")
