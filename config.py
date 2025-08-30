0f3702365d1bdfe4de24c65a5295cd5ce36e06a7
VetalMT <pelene2017@gmail.com>
2025-08-30 22:44:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # токен твого бота
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # id адміна
 
-# URL бази даних PostgreSQL (наприклад, у Render або Heroku)
-DATABASE_URL = os.getenv("DATABASE_URL")
+# PostgreSQL
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
