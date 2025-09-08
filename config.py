e35c88e48659f77b3a18acf2e54e2247b06e7625
VetalMT <pelene2017@gmail.com>
2025-09-08 22:58:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # токен твого бота
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # id адміна
-
-# PostgreSQL
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "469040941"))  # змінити в Render env
+DB_PATH = os.getenv("DB_PATH", "shop.db")
