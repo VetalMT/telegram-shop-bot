e201fb779aa03c8215557db0672b3224fd663107
VetalMT <pelene2017@gmail.com>
2025-09-11 00:06:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
 
-# Обов'язково в env:
-# BOT_TOKEN, ADMIN_ID, RENDER_EXTERNAL_URL, PORT (опційно)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL")
-
-# Шлях до SQLite (у контейнері пишемо у робочу директорію)
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # налаштуй в Render/Env
 DB_PATH = os.getenv("DB_PATH", "shop.db")
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST")  # повинен бути типу https://example.com
