e0f014d772489961eed5d72b0f76923bf277f4eb
VetalMT <pelene2017@gmail.com>
2025-09-12 00:52:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # постав свій ADMIN_ID у Render env
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 DB_PATH = os.getenv("DB_PATH", "shop.db")
-# WEBHOOK_HOST: від Render це часто RENDER_EXTERNAL_URL, або задай свій WEBHOOK_HOST (https://...)
+# WEBHOOK_HOST можна задати як WEBHOOK_HOST або Render дає RENDER_EXTERNAL_URL
 WEBHOOK_HOST = os.getenv("WEBHOOK_HOST") or os.getenv("RENDER_EXTERNAL_URL")
